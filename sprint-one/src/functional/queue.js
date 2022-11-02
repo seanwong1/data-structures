1d847dcd882eab11968fae2ca1353c2113a4f117
Sean Wong <sean.uuong@gmail.com>
2022-11-01 20:34:41 -0700

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,28 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var count = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var key = Object.keys(storage)[0];
+    var value = Object.values(storage)[0];
+    delete storage[key];
+    for (var i = 1; i < someInstance.size; i++) {
+        storage[i - 1] = storage[i];
+        delete storage[i];
+    }
+    return value;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,23 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var count = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    var key = Object.keys(storage)[Object.keys(storage).length - 1];
+    var value = Object.values(storage)[Object.values(storage).length - 1];
+    delete storage[key];
+    return value;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
