c00513fbcf4ad3a026d7499a32143bfea8883add
Sean Wong <sean.uuong@gmail.com>
2022-11-03 20:06:31 -0700

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,22 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+    this.storage = {};
   }
 
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    var key = Object.keys(this.storage)[0];
+    var value = Object.values(this.storage)[0];
+    delete this.storage[key];
+    return value;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,24 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  pop() {
+    var key = Object.keys(this.storage)[Object.keys(this.storage).length - 1];
+    //var key = Object.keys(this.storage)[this.size];
+    //console.log(this.size);
+    var value = Object.values(this.storage)[Object.values(this.storage).length - 1];
+    delete this.storage[key];
+    return value;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
