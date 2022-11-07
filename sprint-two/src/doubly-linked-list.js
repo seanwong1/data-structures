95966035d60baf1bbedcab06aacfdd9ec9aff253
kyle <kyle.dick0811@gmail.com>
2022-11-07 13:01:59 -0700

    working solo on advanced content doubly-linked-list

--- sprint-two/src/doubly-linked-list.js
+++ sprint-two/src/doubly-linked-list.js
@@ -13,21 +13,40 @@ var doublyLinkedList = function() {
       newNode.previous = this.tail;
       this.tail = newNode;
     }
-    console.log(this.tail);
-
   };
-
+  
   list.removeHead = function() {
-
+    var nextHead = this.head.next;
+    var result = this.head.value;
+    this.head.next = null;
+    this.head = nextHead;
+    nextHead.previous = null;
+    return result;
   };
 
   list.contains = function(target) {
+    var node = this.head;
+    while (node) {
+      if (node.value === target) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+    return false;
   };
 
   list.removeTail = function() {
+    var prev = this.tail.previous;
+    prev.next = null;
+    this.tail = prev;
   };
 
   list.addToHead = function(value) {
+    var newNode = new Node(value);
+    newNode.next = this.head;
+    this.head.previous = newNode;
+    this.head = newNode;
   };
 
 
@@ -36,11 +55,9 @@ var doublyLinkedList = function() {
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.previous = null;
   node.next = null;
-
   return node;
 };
 
@@ -48,3 +65,11 @@ var Node = function(value) {
 var test = doublyLinkedList();
 test.addToTail(3);
 test.addToTail(7);
+test.addToTail(19);
+test.addToTail(13);
+test.addToTail(37);
+test.addToHead(777);
+test.removeHead();
+test.removeTail();
+console.log(test);
+console.log(test.contains(19));
